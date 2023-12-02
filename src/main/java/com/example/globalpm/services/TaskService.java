952d927a5f93d0b42824a6fdd15dbcc3182d4137
Ayush <pandeyayushraj4@gmail.com>
2023-12-02 23:49:31 +0000

    Added get Tasks in a project

--- src/main/java/com/example/globalpm/services/TaskService.java
+++ src/main/java/com/example/globalpm/services/TaskService.java
@@ -2,7 +2,9 @@ package com.example.globalpm.services;
 import com.example.globalpm.data.TaskRepository;
 import com.example.globalpm.entities.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
