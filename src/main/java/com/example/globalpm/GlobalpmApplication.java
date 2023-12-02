d92911bc8074b7734269152396974c0984ede230
Ayush <pandeyayushraj4@gmail.com>
2023-12-02 16:48:22 +0000

    First commit

---
+++ src/main/java/com/example/globalpm/GlobalpmApplication.java
@@ -0,0 +1,13 @@
+package com.example.globalpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalpmApplication.class, args);
+	}
+
+}
